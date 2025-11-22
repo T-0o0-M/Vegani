a53233bdc67d5d93560d3c0d4160fce7185f7d86
T-0o0-M <tommaso.vianello.23@gmail.com>
2025-11-22 15:16:54 +0100

    Inziato a lavorare su push e pop, impostato lo scheletro della classe

---
+++ include/InertialDriver.h
@@ -0,0 +1,60 @@
+#ifndef INERTIALDRIVER_H
+#define INERTIALDRIVER_H
+
+#include "myVector.h"
+#include <iostream>
+
+// dimensione buffer arbitraria
+const int BUFFER_DIM = 9; 
+
+// una lettura è composta da 6 valori
+struct Reading {
+    double yaw_v;
+    double yaw_a;
+    double pitch_v;
+    double pitch_a;
+    double roll_v;
+    double roll_a;
+};
+
+// una misura sono 17 letture 
+// così è un doppio struct, Measure contiene Reading che contiene 6 double
+struct Measure {
+    // array stile c di 17 letture
+    Reading sensors[17]; 
+};
+
+class InertialDriver {
+private:
+    // il buffer lo usiamo con myvector, dimensione fissata da BUFFER_DIM
+    myVector<Measure> buffer;
+    
+    // Index del primo slot libero
+    int firstFreeIndex;
+    // Indice della misura meno recente
+    int oldestMeasureIndex;
+    //numero attuale di elementi per doppio controllo
+    int elem_count;
+
+public:
+    InertialDriver();
+
+    // aggiungi una misura; se il buffer è pieno, sovrascrive la misura più vecchia
+    void push_back(const Measure& m);
+
+    // fornisce e rimuove misura più vecchia
+    Measure pop_front();
+
+    // setta il buffer vuoto
+    void clear_buffer();
+
+    // Dalla misura più recente, restituisce una delle 17 letture (composte da i 6 double).
+    // Se sensor_index è fuori dall'intervallo [0,16], il comportamento dipende dall'implementazione:
+    // può generare un'eccezione, restituire una lettura di default, o causare un errore di runtime.
+    Reading get_reading(int sensor_index) const;
+
+    // Overload operator<< print misura più recente (non testato)
+    friend std::ostream& operator<<(std::ostream& os, const InertialDriver& driver);
+};
+
+#endif
---
+++ src/InertialDriver.cpp
@@ -0,0 +1,68 @@
+#include "../include/InertialDriver.h"
+#include <iostream>
+#include <stdexcept>
+
+InertialDriver::InertialDriver() {
+    // Construttore per inizializzazione
+    firstFreeIndex = 0;
+    oldestMeasureIndex = 0;
+    elem_count = 0;
+}
+
+//incrementa gli indici dinamicamente attorno alla max size di 9
+ void InertialDriver::index_Increment(){
+
+    if(firstFreeIndex < 8){firstFreeIndex++;}
+    if(oldestMeasureIndex < 8){oldestMeasureIndex++;}
+
+    if(firstFreeIndex == 8){firstFreeIndex == 0;}
+    if(oldestMeasureIndex == 8){oldestMeasureIndex == 0;}
+ }
+
+// aggiungi una misura; se il buffer è pieno, sovrascrive la misura più vecchia
+void InertialDriver::push_back(const Measure& m) {
+    //dato che myvector è una classe di gestione dinamica
+    //lasciamo il buffer crescere fino alla sua dimensione massima consentita
+    //e solo dopo che sono stati salvati i primi 9 valori utilizziamo gli index circolari
+
+    //caso in cui il buffer non abbai ancora ricevuto i primi 9 valori
+    if (buffer.size() < BUFFER_DIM){
+    buffer.push_back(m);
+
+    elem_count++;
+    //semplice implementazione utilizzando la classe myVector
+    //eleem_count è provvisiorio nel caso sia inutile lo tolgo
+  }
+  //caso in cui siamo arrivati ai primi 9
+  //iniziamo ad utilizziare gli index
+   if (buffer.size() == BUFFER_DIM){
+    //siamo nel caso in cui NON possiamo più permettere al buffer di crescere in size
+    //allora utiliziamo gli index
+    buffer[firstFreeIndex] = m;
+    index_Increment();
+   }
+}
+
+// fornisce e rimuove misura più vecchia
+Measure InertialDriver::pop_front() {
+   Measure rtrn = buffer[oldestMeasureIndex];
+   if(oldestMeasureIndex == 0){oldestMeasureIndex = 8;}
+   if(oldestMeasureIndex > 0){oldestMeasureIndex--;}
+   elem_count--;
+   return rtrn;
+}
+
+ // setta il buffer vuoto
+void InertialDriver::clear_buffer() {
+    
+}
+
+ // dalla misura più recente, restituisce una delle 17 letture (composte da i 6 double)
+Reading InertialDriver::get_reading(int sensor_index) const {
+    
+}
+
+//overloead operatore <<
+std::ostream& operator<<(std::ostream& os, const InertialDriver& driver) {
+  
+}
